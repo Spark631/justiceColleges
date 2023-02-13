f46958f6603e6ce067315aeee07698ad6e30dd0f
Donny Nguyen <donnynguyen631@gmail.com>
2023-02-13 00:53:25 -0500

    Created markers based off of data from firebase

--- colleges/src/components/Map.svelte
+++ colleges/src/components/Map.svelte
@@ -1,35 +1,63 @@
 <script>
   import { onMount } from 'svelte';
+  import { getDatabase, ref, child, get } from "firebase/database";
+  import { initializeApp } from "firebase/app";
+
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyAudXEwwxQfoVNDhDdJYBFXjr4tjxZiIWI",
+    authDomain: "justice-college.firebaseapp.com",
+    databaseURL: "https://justice-college-default-rtdb.firebaseio.com",
+    projectId: "justice-college",
+    storageBucket: "justice-college.appspot.com",
+    messagingSenderId: "384708680296",
+    appId: "1:384708680296:web:9c213e18337580983179dd",
+    databaseURL: "https://justice-college-default-rtdb.firebaseio.com/",
+  };
+
+// Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  
   let map;
 
   function initMap() {
+    const dbRef = ref(getDatabase());
+    const centerUS = { lat: 43.0902, lng: -94.7129 };
 
-//   // The location of Uluru
-      const uluru = { lat: 37.7749, lng: -122.4194 };
-      // The map, centered at Uluru
-      const map = new google.maps.Map(document.getElementById("map"), {
-          zoom: 4,
-          center: uluru,
-      });
-      // The marker, positioned at Uluru
-      const marker = new google.maps.Marker({
-          position: uluru,
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 5,
+        center: centerUS,
+    });
+    
+  get(child(dbRef, `Seniors/`)).then((snapshot) => {
+    if (snapshot.exists()) {
+      console.log(snapshot.val());
+      const dataArray = snapshot.val();
+
+      dataArray.forEach((element) => {
+        console.log("this is the element" + element.First);
+        console.log("this is their position" + element.Lat, element.Lng);
+        const marker = new google.maps.Marker({
+          position: { lat: element.Lat, lng: element.Lng },
           map: map,
           icon: "https://ik.imagekit.io/twkicasuw/Pics/GradBig.png?ik-sdk-version=javascript-1.4.3&updatedAt=1675658089618"
       });
-      const marker1 = new google.maps.Marker({
-          position: { lat: 40, lng: -150 },
-          map: map,
-          icon: "https://ik.imagekit.io/twkicasuw/Pics/GradBig.png?ik-sdk-version=javascript-1.4.3&updatedAt=1675658089618"
       });
+      
+    } else {
+      console.log("No data available");
+    }
+  }).catch((error) => {
+    console.error(error);
+  });
   }
 
   onMount(async () => {
     const script = document.createElement('script');
     script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyA8g5g4017HkLoY-2B9x1DIQd4iFXdGJIE&callback=initMap`;
-  //   script.src = `https://maps.googleapis.com/maps/api/staticmap?center=40.714728,-73.998672&zoom=12&size=400x400&maptype=satellite&key=AIzaSyA8g5g4017HkLoY-2B9x1DIQd4iFXdGJIE`;
     document.body.appendChild(script);
   });
+
 </script>
   
   <style>
